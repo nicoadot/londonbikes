43009d49e190117289ccebe901040a83618c2180
Nicolas Adot <nico_adot@hotmail.com>
2018-06-19 11:57:27 -0300

    inicializar repositorio

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('saludar/{nombre}/{apellido}', function ($nombre,$apellido) {
+    echo("aloha ".$nombre);
+});
+
+//Route::get('la ruta que invoco', 'controlador@action');
+Route::get('bienvenido', 'HomeController@index');
